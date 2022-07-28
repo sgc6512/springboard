13b6e9e79b58cb5a2158d62a2c42903b2eb0888c
sgc6512 <81432815+sgc6512@users.noreply.github.com>
2022-07-28 17:37:33 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,71 @@
-function countdown(num) {
-    let intID = setInterval(function() {
-        display(num);
-        num--;
-        if (num < 0) {
-            clearInterval(intID);
-        }
-    }, 1000)
+const formElement = document.querySelector("form");
+const taskList = document.querySelector("ul");
+var savedList = JSON.parse(localStorage.getItem("list")) || [];
+
+
+for(let i = 0; i < savedList.length; i++) {
+    addToList(savedList[i].task, false, savedList[i].done);
 }
 
-function display(num) {
-    if(num === 0) {
-        console.log("DONE!");
+
+formElement.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const input = document.querySelector("#listItem");
+    addToList(input.value, true, false);
+    formElement.reset();
+})
+
+taskList.addEventListener("click", function(e) {
+    if(e.target.tagName === "INPUT") {
+        if(e.target.checked)
+            e.target.parentElement.style.textDecoration = "line-through";
+        else
+            e.target.parentElement.style.textDecoration = "";
     }
-    else
-        console.log(num);
-}
-
-function randomGame() {
-    let count = 0;
-    let intID = setInterval(function () {
-        count++;
-        let num = Math.random();
-        if(num > 0.75) {
-            console.log(count);
-            clearInterval(intID);
+    if(e.target.tagName === "BUTTON") {
+        let toDelete;
+        for(let i = 0; i < savedList.length; i++) {
+            if(savedList[i].task + 'X' === e.target.parentElement.innerText) {
+                toDelete = i;
+            }
         }
-    }, 1000)
+        savedList.splice(toDelete, 1);
+        e.target.parentElement.remove();
+    }
+
+    doneCheck(e.target);
+    localStorage.setItem("list", JSON.stringify(savedList));
+})
+
+function addToList(input, save, done) {
+    const newLi = document.createElement("li");
+    const newButton = document.createElement("button");
+    const newCheck = document.createElement("input");
+    newCheck.setAttribute("type", "checkbox");
+
+    if(done) {
+        newLi.style.textDecoration = "line-through";
+        newCheck.checked = true;
+    }
+
+    newButton.innerText = "X";
+    newLi.innerText = input;
+    newLi.append(newButton);
+    newLi.prepend(newCheck);
+    taskList.append(newLi);
+
+    if(save) {
+        savedList.push({task: input, done: done})
+        localStorage.setItem("list", JSON.stringify(savedList));
+    }
+}
+// Duplicates don't work with this. But as far as I can tell everything else does.
+function doneCheck(input) {
+    for(let i = 0; i < savedList.length; i++) {
+        if(savedList[i].task + 'X' === input.parentElement.innerText) {
+            if(savedList[i].done != input.checked) {
+                savedList[i].done = !savedList[i].done;
+            }
+        }
+    }
 }
